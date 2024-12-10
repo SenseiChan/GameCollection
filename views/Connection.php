7ef92d9ef9c58f346ebb7b62e66fc840b9d6dfff
W4nou <wanou35.foulon@laposte.net>
2024-12-10 14:43:36 +0100

    add of the view Connection page

---
+++ views/Connection.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Game Collection | Connexion</title>
+    <link rel="stylesheet" href="asset/style.css">
+<!--    <link rel="icon" href="./favicon.ico" type="image/x-icon">-->
+</head>
+<body>
+<main>
+    <h1>Se connecter à Game Collection</h1>
+    <form action="" method="POST">
+        <p>Email :</p>
+        <input type="email" name="email">
+        <p>Mot de passe :</p>
+        <input type="password" name="password">
+        <input type="button" name="submit" value="Se connecter">
+    </form>
+    <button name="signup">S'inscrire</button>
+</main>
+</body>
+</html>
+
