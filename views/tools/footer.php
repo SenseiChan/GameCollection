208b869cf3ebf10cdd5c9d7b0699a852d350225b
Evann Congnard <econgnard@gmail.com>
2024-12-10 15:40:22 +0100

    tooooools

---
+++ views/tools/footer.php
@@ -0,0 +1,6 @@
+<footer class="site-footer">
+    <link rel="stylesheet" href="footerstyle.css">
+    <div class="footer-content">
+        Game Collection - 2023 - Tous droits réservés
+    </div>
+</footer>
---
+++ views/tools/header.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="header-container">
+        <!-- Logo -->
+        <div class="logo">
+            <img src="" alt="Logo">
+        </div>
+
+        <!-- Navigation -->
+        <nav>
+            <div class="nav-links">
+                <a href="">Ma bibliothèque</a>
+                <a href="">Ajouter un jeu</a>
+                <a href="">Boutique</a>
+                <a href="">BDE</a>
+                <a href="">FAQ</a>
+            </div>
+        </nav>
+    </div>
+</header>
